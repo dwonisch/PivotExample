e75ef4da75257698e87fb6edf7637d4ba1e5b408
Daniel Wonisch <daniel.wonisch@gmail.com>
2014-03-02 23:34:09 +0100

    Preparations for optimizing CalculatePivot method

--- src/PivotExample/Pivot.cs
+++ src/PivotExample/Pivot.cs
@@ -16,17 +16,14 @@
         /// <returns>A Pivot Table of the queried data.</returns>
         public DataTable CreatePivot(IDataReader reader, IList<ColumnConfiguration> configuration) {
             var watch = Stopwatch.StartNew();
+            var returnTable = new DataTable();            
+
             var columnDefinitions = new Dictionary<Tuple<string,string>,ColumnDefinition>();
             var configurationDictionary = configuration.ToDictionary(c => c.Name);
-            var data = FetchData(reader, configurationDictionary, columnDefinitions);
+            var data = FetchData(reader, configurationDictionary, columnDefinitions, returnTable);
             Console.WriteLine("FetchData: {0}", watch.ElapsedMilliseconds);
             watch.Reset();
             watch.Start();
-            var returnTable = CreateTableDefinition(columnDefinitions);
-            Console.WriteLine("CreateTableDefinition: {0}", watch.ElapsedMilliseconds);
-            watch.Reset();
-            watch.Start();
-
             CalculatePivot(returnTable, data, configuration);
             Console.WriteLine("CalculatePivot: {0}", watch.ElapsedMilliseconds);
             watch.Reset();
@@ -43,17 +40,22 @@
         /// <param name="reader">The IDataReader from which the data is read.</param>
         /// <param name="configuration">The configuration of all columns that should be contained in Pivot Table.</param>
         /// <param name="columns">A collection of columns that is filled by this method.</param>
+        /// <param name="returnTable">The DataTable where generated columns are added.</param>
         /// <returns>A List of DataObject that contains each row's data.</returns>
-        private static IList<DataObject> FetchData(IDataReader reader, IDictionary<string, ColumnConfiguration> configuration, IDictionary<Tuple<string,string>, ColumnDefinition> columns) {
+        private static IList<DataObject> FetchData(IDataReader reader, IDictionary<string, ColumnConfiguration> configuration, IDictionary<Tuple<string, string>, ColumnDefinition> columns, DataTable returnTable) {
             //Add first data column by which all values are grouped by
-            columns.Add(Tuple.Create("PARTID", "PARTID"), new ColumnDefinition("PARTID", 0, typeof(string)));
+            var primaryKeyColumn = new ColumnDefinition("PARTID", 0, typeof(string));
+            columns.Add(Tuple.Create("PARTID", "PARTID"), primaryKeyColumn);
+            CreateTableDefinition(returnTable, primaryKeyColumn);
 
             var dataObjects = new List<DataObject>();
 
             while (reader.Read()) {
                 var dataObject = new DataObject(reader);
                 dataObjects.Add(dataObject);
-                GetColumns(columns, configuration, dataObject);
+                var columnDefintion = GetColumns(columns, configuration, dataObject);
+                if (columnDefintion != null)
+                    CreateTableDefinition(returnTable, columnDefintion);
             }
 
             return dataObjects;
@@ -62,36 +64,30 @@
         /// <summary>
         /// Create a column definition for each data row combined with configuration data.
         /// </summary>
-        /// <param name="data">All rows that were returned from database.</param>
+        /// <param name="columns">The collection of the currently available columns. New definitions are added here.</param>
         /// <param name="configuration">The definition in which order and what columns to return.</param>
         /// <param name="dataEntry">The entry that is used to create additional columns.</param>
         /// <returns>A List of column definitions.</returns>
-        private static void GetColumns(IDictionary<Tuple<string, string>, ColumnDefinition> columns, IDictionary<string, ColumnConfiguration> configuration, DataObject dataEntry) {
+        private static ColumnDefinition GetColumns(IDictionary<Tuple<string, string>, ColumnDefinition> columns, IDictionary<string, ColumnConfiguration> configuration, DataObject dataEntry) {
             if (configuration.ContainsKey(dataEntry.ValueId)) {
                 var configurationEntry = configuration[dataEntry.ValueId];
                 string displayName = configurationEntry.DisplayName;
                 var key = Tuple.Create(dataEntry.ValueId, displayName);
                 if (!columns.ContainsKey(key)) {
                     columns.Add(key, new ColumnDefinition(displayName, configurationEntry.SortOrder, configurationEntry.DataType));
+                    return columns[key];
                 }
             }
+            return null;
         }
 
         /// <summary>
         /// Converts a List of ColumnDefinition into a DataTable
         /// </summary>
-        /// <param name="columnDefinitions">For each column definition a DataColumn is added to the returned DataTable.</param>
-        /// <returns>A DataTable based on the committed column definitions.</returns>
-        private DataTable CreateTableDefinition(IDictionary<Tuple<string, string>, ColumnDefinition> columnDefinitions) {
-            var sortedDefinitions = columnDefinitions.Values.OrderBy(d => d.SortOrder).ToList();
-
-            var dataTable = new DataTable();
-
-            foreach (var columnDefinition in sortedDefinitions) {
-                dataTable.Columns.Add(columnDefinition.DisplayName, columnDefinition.DataType);
-            }
-
-            return dataTable;
+        /// <param name="dataTable">The DataTable where the generated column is added.</param>
+        /// <param name="columnDefinition">The information that is used to create the columnDefinition.</param>
+        private static void CreateTableDefinition(DataTable dataTable, ColumnDefinition columnDefinition) {
+            dataTable.Columns.Add(columnDefinition.DisplayName, columnDefinition.DataType);
         }
 
         /// <summary>
